c23a996f2513607808ee69a941ca4adceb9d1418
harshit <harshit2k19@gmail.com>
2024-07-24 12:16:03 +0530

    Added async into root

--- backend.py
+++ backend.py
@@ -6,7 +6,7 @@ from fastapi.responses import JSONResponse
 app = FastAPI()
 data_df=pd.read_excel('data.xlsx')
 @app.get("/")
-def root():
+async def root():
     json_str = data_df.to_json(orient="records")
     records = json.loads(json_str)
     return JSONResponse(content=records)
